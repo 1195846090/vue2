0c16d2a0e9462955ea4600a6ac443654049d568b
梅俊杰 <1195846090@qq.com>
2021-01-23 11:50:43 +0800

    login of compent

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,70 +1,95 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="avater_box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-            <!-- 登录表单 -->
-        <el-form ref="loginFormRef" label-width="0px" :rules="loginFormRules" :model="loginForm" class="login_form">
-            <!-- 登录 -->
-             <el-form-item prop="username">
-             <el-input  v-model="loginForm.username" prefix-icon="iconfont icon-user" ></el-input>
-             </el-form-item>
-             
-             <!-- 密码 -->
-             <el-form-item prop="password">
-             <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
-             </el-form-item>
-             <!-- 按钮 -->
-             <el-form-item class="btns">
-         <el-button type="primary" @click="login">登录</el-button>
-         <el-button type="info" @click="resetLoginForm">重置</el-button>
-             </el-form-item>
-        </el-form>
-        </div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单 -->
+      <el-form
+        ref="loginFormRef"
+        label-width="0px"
+        :rules="loginFormRules"
+        :model="loginForm"
+        class="login_form"
+      >
+        <!-- 登录 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            // 登录表单的数据绑定对象
-            loginForm: 
-            {
-                username: '',
-                password: ''
-            },
-            // 表单验证规则对象
-            loginFormRules:
-            {
-                // 验证用户名是否合法
-                username:[
-                     { required: true, message: '请输入登录名称', trigger: 'blur' },
-                     { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
-                ],
-                // 验证密码是否合法
-                password:[
-                     { required: true, message: '请输入登录密码', trigger: 'blur' },
-                     { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
-                ]
-            }
-        }
-    },
-    methods: {
-        resetLoginForm(){
-            this.$refs.loginFormRef.resetFields();
-        },
-        login(){
-            this.$refs.loginFormRef.validate(async valid => {
-               if(!valid) return;
-              const { data: res } = await this.$http.post("login", this.loginForm);
-              console.log(res);
-            })
-        }
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456',
+      },
+      // 表单验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur',
+          },
+        ],
+      },
     }
-};
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登陆失败')
+        this.$message.success('登陆成功')
+
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
@@ -103,14 +128,14 @@ export default {
   }
 }
 .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
