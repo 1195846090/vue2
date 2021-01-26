a86e20749ec7c6cb8aec310baeacb6e7c7926f75
梅俊杰 <1195846090@qq.com>
2021-01-26 10:37:21 +0800

    分类copment

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
+
+const Login = () => import('../components/Login.vue')
+
+const Home = () => import('../components/Home.vue')
+
+const Welcome = () => import('../components/Welcome.vue')
+
+const Users = () => import('../components/user/Users.vue')
+
+const Rights = () => import('../components/power/Rights.vue')
+
+const Roles = () => import('../components/power/Roles.vue')
+const Cate = () => import('../components/goods/Cate.vue')
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+// import Users from '../components/user/Users.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Roles.vue'
+// // import Add from '../components/goods/Add.vue'
+// import Cate from '../components/goods/Cate.vue'
+// // import List from '../components/goods/List.vue'
+// // import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -20,7 +37,11 @@ const routes = [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: Users },
       { path: '/rights', component: Rights },
-      { path: '/roles', component: Roles }
+      { path: '/roles', component: Roles },
+      // { path: '/roles', component: Add },
+      { path: '/categories', component: Cate },
+      // { path: '/roles', component: List },
+      // { path: '/roles', component: Params },
     ]
   },
  
